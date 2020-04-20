34734dfc396b37911672c08e62b053a8e4a7dfdd
eLeontev <joonior199@gmail.com>
2020-04-21 00:37:29 +0300

    implement core express

--- packages/express/express.ts
+++ packages/express/express.ts
@@ -1,8 +1,89 @@
-class Express {
-    public a: number;
-    constructor() {
-        this.a = 10;
+import {
+    Express,
+    RouteListener,
+    MiddleWareListener,
+    Routes,
+    methods,
+    Request,
+} from './express.model';
+
+class App implements Express {
+    private port: number;
+    private routes: Routes = {
+        [methods.GET]: {},
+        [methods.POST]: {},
+        [methods.MIDDLEWARE]: {},
+    };
+
+    listen(port: number) {
+        this.port = port;
+    }
+
+    get(url: string, routeListener: RouteListener) {
+        this.routes[methods.GET][url] = routeListener;
+    }
+
+    post(url: string, routeListener: RouteListener) {
+        this.routes[methods.POST][url] = routeListener;
+    }
+
+    use(url: string, middleWareListener: MiddleWareListener) {
+        const routes = this.routes[methods.MIDDLEWARE];
+        if (!routes[url]) {
+            routes[url] = [];
+        }
+
+        routes[url] = [...routes[url], middleWareListener];
+    }
+
+    performRequestLogic(
+        method: methods.GET | methods.POST,
+        url: string,
+        request: Request,
+        response: Response
+    ) {
+        const { routes, getMiddlewaresResult, port } = this;
+        const listener = routes[method][url];
+        const middlewares = routes[methods.MIDDLEWARE][url] || [];
+
+        if (!listener || !port) {
+            return;
+        }
+
+        const { req, res, shouldCallListener } = getMiddlewaresResult(
+            middlewares,
+            request,
+            response
+        );
+
+        if (shouldCallListener) {
+            listener(req, res);
+        }
+    }
+
+    private getMiddlewaresResult(
+        listeners: Array<MiddleWareListener>,
+        req: Request,
+        res: Response
+    ) {
+        let shouldCallListener = true;
+        const next = () => (shouldCallListener = true);
+
+        listeners.forEach((listener: MiddleWareListener) => {
+            shouldCallListener = false;
+            listener(req, res, next);
+        });
+
+        return {
+            req,
+            res,
+            shouldCallListener,
+        };
     }
 }
 
-export { Express };
+const express: () => Express = () => {
+    return new App();
+};
+
+export { express };
