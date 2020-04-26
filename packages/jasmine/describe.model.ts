1b1b98b5771162b0312b38cb57126ebbf360d053
eLeontev <joonior199@gmail.com>
2020-04-26 14:40:15 +0300

    extend itList structure, add missed interface

--- packages/jasmine/describe.model.ts
+++ packages/jasmine/describe.model.ts
@@ -1,11 +1,15 @@
-import { CallbackList, Callback } from './jasmine.model';
+import { CallbackList, Callback, DescribeModel } from './jasmine.model';
 
+export interface It {
+    description: string;
+    callback: Callback;
+}
 export interface Describer {
     description: string;
     beforeEachList: CallbackList;
     afterEachList: CallbackList;
     childrenDescribersId: Array<string>;
-    itList: CallbackList;
+    itList: Array<It>;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     context: any;
 }
@@ -17,4 +21,28 @@ export interface Describers {
 export interface NextDescriberArguments {
     description: string;
     callback: Callback;
+}
+
+export interface InnerMethods {
+    describe: DescribeModel;
+    addChildDesriberId(describerId: string, childDescriberId: string): void;
+    setFormedDescriber(
+        describer: Describer,
+        describerId: string,
+        isRootDescriber: boolean
+    ): void;
+    performChildrenDescribers(describerId: string): void;
+    initDescribe(description: string, describerId?: string): string;
+    afterCallbackCall(): void;
+    beforeCallbackCall(description: string, describerId: string): void;
+    describeHandler(
+        description: string,
+        callback: Callback,
+        describerId?: string
+    ): void;
+    childDescribe(
+        description: string,
+        callback: Callback,
+        describerId: string
+    ): void;
 }
