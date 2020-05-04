e47030780024e8b7c7b478d88f1ffeec9d598e49
eLeontev <joonior199@gmail.com>
2020-05-04 23:08:29 +0300

    actualize errors to work with negative validators

--- packages/jasmine/error.messages.ts
+++ packages/jasmine/error.messages.ts
@@ -5,17 +5,35 @@ import {
 } from './models/error.messages.model';
 import { ExpectedResult, ActualResult } from './models/matchers.model';
 
+export const getNotMessage = (isNot: boolean): string =>
+    isNot ? ' not ' : ' ';
+
 export const testTimeFrameDurationExeeded: ErrorMessageCallback = (
+    isNot: boolean,
     actualResult: ActualResult
 ) => `async test takes more than available ${JSON.stringify(actualResult)}ms`;
+
 export const expectDoNothing: ErrorMessageCallback = (
+    isNot: boolean,
     actualResult: ActualResult
 ) =>
     `looks like this expect does nothing with: ${JSON.stringify(actualResult)}`;
-export const toBeFalsy: ErrorMessageCallback = (actualResult: ActualResult) =>
-    `expected ${JSON.stringify(actualResult)} to be falsy`;
-export const toBeTruthy: ErrorMessageCallback = (actualResult: ActualResult) =>
-    `expected ${JSON.stringify(actualResult)} to be truthy`;
+
+export const toBeFalsy: ErrorMessageCallback = (
+    isNot: boolean,
+    actualResult: ActualResult
+) =>
+    `expected ${JSON.stringify(actualResult)}${getNotMessage(
+        isNot
+    )}to be falsy`;
+
+export const toBeTruthy: ErrorMessageCallback = (
+    isNot: boolean,
+    actualResult: ActualResult
+) =>
+    `expected ${JSON.stringify(actualResult)}${getNotMessage(
+        isNot
+    )}to be truthy`;
 
 export const errorMessages: ErrorMessages = {
     testTimeFrameDurationExeeded,
@@ -27,7 +45,8 @@ export const errorMessages: ErrorMessages = {
 export const getErrorMessage: GetErrorMessage = (
     isSuccess: boolean,
     errorMessageCallback: ErrorMessageCallback,
+    isNot: boolean,
     actualResult: ActualResult,
     expectedResult?: ExpectedResult
 ): string =>
-    isSuccess ? '' : errorMessageCallback(actualResult, expectedResult);
+    isSuccess ? '' : errorMessageCallback(isNot, actualResult, expectedResult);
