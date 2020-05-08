c4dc519c6622a891a5d6e71dc506f2cdf0720f61
eLeontev <joonior199@gmail.com>
2020-05-08 21:27:40 +0300

    move isSpy method out ot reuse

--- packages/jasmine/models/utils.model.ts
+++ packages/jasmine/models/utils.model.ts
@@ -1,5 +1,6 @@
 import { ActualResult } from './matchers.model';
 
+export type IsSpy = (actualResult: ActualResult) => boolean;
 export type GetActualResultWithSpy = (
     actualResult: ActualResult
 ) => ActualResult;
--- packages/jasmine/utils.ts
+++ packages/jasmine/utils.ts
@@ -1,13 +1,16 @@
 import { ActualResult } from './models/matchers.model';
 import { SpyAPICore } from './models/spy-api.model';
-import { GetActualResultWithSpy } from './models/utils.model';
+import { GetActualResultWithSpy, IsSpy } from './models/utils.model';
+
+export const isSpy: IsSpy = (actualResult: ActualResult): boolean =>
+    actualResult && Boolean(actualResult.getSpyResult);
 
 export const getActualResultWithSpy: GetActualResultWithSpy = (
     actualResult: ActualResult
 ): ActualResult => {
     let result = actualResult;
 
-    if (actualResult && actualResult.getSpyResult) {
+    if (isSpy(actualResult)) {
         result = (actualResult as SpyAPICore).getSpyResult();
     }
 
