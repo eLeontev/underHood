1d552428ec07d48eab297b3957249e2ccb920378
eLeontev <joonior199@gmail.com>
2020-05-10 14:58:21 +0300

    add integration tests for new  spyMethod

--- packages/jasmine/tests/jasmine.spy.intergattion.test.ts
+++ packages/jasmine/tests/jasmine.spy.intergattion.test.ts
@@ -3,20 +3,27 @@
 import { Jasmine } from '../jasmine';
 import { Spy } from '../spy';
 import { ValidatorResult } from '../models/matchers.model';
+import { SpyMethod } from '../models/spy.model';
 
 const inst = new Jasmine();
-const { createSpy } = new Spy();
+const { createSpy, spyMethod } = new Spy();
 
 const fn = (): any => true;
 const args = [1, 2, 3, {}, fn];
 
 let callableSpy: any;
 let uncallableSpy: any;
+let spiedMethod: SpyMethod;
 
 inst.describe('test', () => {
     inst.it('spy integration tests', () => {
+        const obj = {
+            fn: (): any => 123,
+        };
+
         callableSpy = createSpy('callableSpy');
         uncallableSpy = createSpy('uncallableSpy');
+        spiedMethod = spyMethod(obj, 'fn');
 
         callableSpy();
         callableSpy(fn);
@@ -29,6 +36,9 @@ inst.describe('test', () => {
         inst.expect(uncallableSpy).not.toHaveBeenCalled();
         inst.expect(undefined).toHaveBeenCalled();
         inst.expect(false).toHaveBeenCalledWith(...args);
+
+        inst.expect(spiedMethod()).toBeFalsy();
+        inst.expect(spiedMethod.callOrigin()()).toBe(123);
     });
 });
 const result = inst.run();
@@ -85,6 +95,16 @@ describe('spy integration tests', () => {
         expect(await getValidatorResultByIndex(6)).toEqual({
             errorMessage: 'expected spy but got false',
             isSuccess: false,
+        });
+
+        expect(await getValidatorResultByIndex(7)).toEqual({
+            errorMessage: '',
+            isSuccess: true,
+        });
+
+        expect(await getValidatorResultByIndex(8)).toEqual({
+            errorMessage: '',
+            isSuccess: true,
         });
     });
 });
