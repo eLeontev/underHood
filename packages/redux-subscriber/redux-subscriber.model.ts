0c4fa71720bbd58ef8d9339a5ec7092fedcfd5f3
eLeontev <joonior199@gmail.com>
2020-05-17 20:24:39 +0300

    move model out to file with models

--- packages/redux-subscriber/redux-subscriber.model.ts
+++ packages/redux-subscriber/redux-subscriber.model.ts
@@ -5,11 +5,36 @@ import {
     BaseAction,
 } from '../redux/redux.model';
 
+export enum ListenerTypes {
+    customListener = 'customListener',
+    actionListener = 'actionListener',
+}
+
+export interface CustomListenerData<Listener> {
+    listener: Listener;
+    pathToListenValue: string;
+    listenerType: ListenerTypes;
+}
+
+export interface ActionListenerData<Listener> {
+    listener: Listener;
+    actionsToFire: Array<string>;
+    listenerType: ListenerTypes;
+}
+
+export type CommonListenerData<T> =
+    | CustomListenerData<T>
+    | ActionListenerData<T>;
+export type ListenersDataStore<T> = Array<CommonListenerData<T>>;
+
 export interface ReduxSubscriber<State, Action extends BaseAction>
     extends ReduxStore<State, Action> {
     setCustomListener(
         pathToListenValue: string,
-        listener: Subscriber<State>,
-        actionsToFire: Array<string>
+        listener: Subscriber<State>
+    ): Unsubscriber;
+    setActionListener(
+        actionsToFire: Array<string>,
+        listener: Subscriber<State>
     ): Unsubscriber;
 }
