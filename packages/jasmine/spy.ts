9efd791f87f5fd9e16e6fa963682166f8206ea73
eLeontev <joonior199@gmail.com>
2020-05-20 20:15:48 +0300

    adapt spy implemenation for new model

--- packages/jasmine/spy.ts
+++ packages/jasmine/spy.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import {
     SpyCore,
     SpyMethod,
@@ -9,36 +11,72 @@ import {
 } from './models/spy.model';
 
 export class Spy implements SpyCore {
-    private spyPropertiesMap: Map<SpyMethod, SpyProperties> = new Map();
+    private spyPropertiesMap: Map<
+        SpyMethod<any, any>,
+        SpyProperties<any, any>
+    > = new Map();
 
     public spyMethod = <
-        Fn extends Function,
-        Context extends { [method: string]: Fn },
+        SpyArguments,
+        Result,
+        Context extends {
+            [method: string]: (...spyArguments: Array<SpyArguments>) => Result;
+        },
         MethodName extends keyof Context
     >(
         context: Context,
-        methodName: MethodName
-    ): SpyMethod => {
-        const spyMethod = this.getNewRegisterSpy(methodName as string);
-        this.setOrigin<Fn, Context, MethodName>(spyMethod, context, methodName);
+        methodName: MethodName,
+        spyArguments: Array<SpyArguments> = [],
+        result: Result = undefined
+    ): SpyMethod<SpyArguments, Result> => {
+        const spyMethod = this.getNewRegisterSpy<SpyArguments, Result>(
+            methodName as string,
+            spyArguments,
+            result
+        );
+        this.setOrigin<SpyArguments, Result, Context, MethodName>(
+            spyMethod,
+            context,
+            methodName
+        );
         spyMethod.callOrigin = this.getCallOrigin(spyMethod);
 
         return spyMethod;
     };
 
-    public createSpy = (spyName: string): SpyMethod => {
-        return this.getNewRegisterSpy(spyName);
+    public createSpy = <SpyArguments, Result>(
+        spyName: string,
+        spyArguments: Array<SpyArguments> = [],
+        result: Result = undefined
+    ): SpyMethod<SpyArguments, Result> => {
+        return this.getNewRegisterSpy<SpyArguments, Result>(
+            spyName,
+            spyArguments,
+            result
+        );
     };
 
-    private getNewRegisterSpy(spyName: string): SpyMethod {
-        const spyProperties = this.initSpyProperties(spyName);
-        return this.initSpy(spyProperties);
+    private getNewRegisterSpy<SpyArguments, Result>(
+        spyName: string,
+        spyArguments: Array<SpyArguments>,
+        result: Result
+    ): SpyMethod<SpyArguments, Result> {
+        const spyProperties = this.initSpyProperties<SpyArguments, Result>(
+            spyName,
+            spyArguments,
+            result
+        );
+        return this.initSpy<SpyArguments, Result>(spyProperties);
     }
 
-    private initSpyProperties(spyName: string): SpyProperties {
+    private initSpyProperties<SpyArguments, Result>(
+        spyName: string,
+        spyArguments: Array<SpyArguments>,
+        result: Result
+    ): SpyProperties<SpyArguments, Result> {
         return {
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            handler: (): void => {},
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            handler: (...spyArguments: Array<SpyArguments>): Result => result,
             spyName,
             isCalled: false,
             countOfCalls: 0,
@@ -46,36 +84,36 @@ export class Spy implements SpyCore {
         };
     }
 
-    private initSpy(spyProperties: SpyProperties): SpyMethod {
-        const spyMethod: SpyMethod = <HandlerResult>(
-            ...args: Array<SpyArguments>
-        ): HandlerResult =>
-            this.perfromSpyCall<HandlerResult, SpyArguments>(spyMethod, args);
+    private initSpy<SpyArguments, Result>(
+        spyProperties: SpyProperties<SpyArguments, Result>
+    ): SpyMethod<SpyArguments, Result> {
+        const spyMethod: SpyMethod<SpyArguments, Result> = <HandlerResult>(
+            ...spyArguments: Array<SpyArguments>
+        ): Result =>
+            this.perfromSpyCall<SpyArguments, Result>(spyMethod, spyArguments);
 
         spyMethod.returnValue = this.getReturnValue(spyMethod);
         spyMethod.callFake = this.getCallFake(spyMethod);
 
-        spyMethod.getSpyProperties = this.getSpyProperties.bind(
-            this,
-            spyMethod
-        );
+        spyMethod.getSpyProperties = (): SpyProperties<SpyArguments, Result> =>
+            this.getSpyProperties<SpyArguments, Result>(spyMethod);
 
         this.registerSpyMethod(spyMethod, spyProperties);
 
-        return spyMethod as SpyMethod;
+        return spyMethod as SpyMethod<SpyArguments, Result>;
     }
 
-    private registerSpyMethod(
-        spyMethod: SpyMethod,
-        spyProperties: SpyProperties
+    private registerSpyMethod<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>,
+        spyProperties: SpyProperties<SpyArguments, Result>
     ): void {
         this.spyPropertiesMap.set(spyMethod, spyProperties);
     }
 
-    private perfromSpyCall<HandlerResult, SpyArguments>(
-        spyMethod: SpyMethod,
+    private perfromSpyCall<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>,
         args: Array<SpyArguments>
-    ): HandlerResult {
+    ): Result {
         const spyProperties = this.getSpyProperties(spyMethod);
 
         spyProperties.countOfCalls = spyProperties.countOfCalls + 1;
@@ -85,9 +123,15 @@ export class Spy implements SpyCore {
         return spyProperties.handler(...args);
     }
 
-    private getCallFake(spyMethod: SpyMethod): CallFake {
-        return (cb: Function): SpyMethod => {
-            const spyProperties = this.getSpyProperties(spyMethod);
+    private getCallFake<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>
+    ): CallFake<SpyArguments, Result> {
+        return (
+            cb: (...spyArguments: Array<SpyArguments>) => Result
+        ): SpyMethod<SpyArguments, Result> => {
+            const spyProperties = this.getSpyProperties<SpyArguments, Result>(
+                spyMethod
+            );
 
             spyProperties.handler = cb;
 
@@ -95,19 +139,28 @@ export class Spy implements SpyCore {
         };
     }
 
-    private getReturnValue(spyMethod: SpyMethod): ReturnValue {
-        return <V>(value: V): SpyMethod => {
+    private getReturnValue<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>
+    ): ReturnValue<SpyArguments, Result> {
+        return (value: Result): SpyMethod<SpyArguments, Result> => {
             const spyProperties = this.getSpyProperties(spyMethod);
 
-            spyProperties.handler = (): V => value;
+            spyProperties.handler = (
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                ...spyArguments: Array<SpyArguments>
+            ): Result => value;
 
             return spyMethod;
         };
     }
 
-    private getCallOrigin(spyMethod: SpyMethod): CallOrigin {
-        return (): SpyMethod => {
-            const spyProperties = this.getSpyProperties(spyMethod);
+    private getCallOrigin<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>
+    ): CallOrigin<SpyArguments, Result> {
+        return (): SpyMethod<SpyArguments, Result> => {
+            const spyProperties = this.getSpyProperties<SpyArguments, Result>(
+                spyMethod
+            );
 
             spyProperties.handler = spyProperties.origin;
 
@@ -116,15 +169,24 @@ export class Spy implements SpyCore {
     }
 
     private setOrigin<
-        Fn extends Function,
-        Context extends { [method: string]: Fn },
+        SpyArguments,
+        Result,
+        Context extends {
+            [method: string]: (...spyArguments: Array<SpyArguments>) => Result;
+        },
         MethodName extends keyof Context
-    >(spyMethod: SpyMethod, context: Context, methodName: MethodName): void {
+    >(
+        spyMethod: SpyMethod<SpyArguments, Result>,
+        context: Context,
+        methodName: MethodName
+    ): void {
         const spyProperties = this.getSpyProperties(spyMethod);
         spyProperties.origin = context[methodName];
     }
 
-    private getSpyProperties(spyMethod: SpyMethod): SpyProperties {
+    private getSpyProperties<SpyArguments, Result>(
+        spyMethod: SpyMethod<SpyArguments, Result>
+    ): SpyProperties<SpyArguments, Result> {
         return this.spyPropertiesMap.get(spyMethod);
     }
 }
